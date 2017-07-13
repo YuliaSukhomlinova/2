dc8a1966d56df45a38a93b93039c6208a92c2e82
YuliaSukhomlinova <yulia_alekseeva1997@mail.ru>
2017-07-13 10:19:52 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //определим переменные х,у-координаты точки.
+            //переменные имеют тип float.
+            double x, y,r;
+            //определим строковые переменные сообщений.
+             string inside = "точка внутри контура";
+             string outside = "точка вне контура";
+            //вносим координаты точки.
+            Console.WriteLine("Введите абциссу точки");
+            x = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите ординату точки");
+            y = Convert.ToDouble(Console.ReadLine());
+            r = (Math.Pow(x, 2) + Math.Pow(y, 2));
+           
+            if (x > 0)
+            {
+                if (r >= 0.5 && r <= 1) Console.WriteLine("inside");
+                else Console.WriteLine("outside");
+            }
+            if (x <= 0)
+            {
+                if (r > 1) Console.WriteLine("outside");
+                else Console.WriteLine("inside");
+            }
+            Console.ReadKey();
+        }
+    }
+}
